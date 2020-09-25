9ceba6e2909a1548120d041d1050912b559c1ca9
Aleksandar <aca801@gmail.com>
2020-09-25 16:25:22 +0200

    Create test actor as user

---
+++ DDDT.API/Core/FakeApiActor.cs
@@ -0,0 +1,17 @@
+﻿using DDDT.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDDT.API.Core
+{
+    public class FakeApiActor : IApplicationActor
+    {
+        public int Id => 1;
+
+        public string Identity => "Test api user";
+
+        public IEnumerable<int> AllowedUseCases => new List<int> { 1 };
+    }
+}
