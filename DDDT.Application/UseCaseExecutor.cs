d111f0f851b47bfda6173fc1caf9516f4936b53a
Aleksandar <aca801@gmail.com>
2020-09-25 17:49:13 +0200

    Add unauthorized usecase exception

--- DDDT.Application/UseCaseExecutor.cs
+++ DDDT.Application/UseCaseExecutor.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using DDDT.Application.Exceptions;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DDDT.Application
@@ -19,6 +21,9 @@ namespace DDDT.Application
             TRequest request)
         {
             Console.WriteLine($"{DateTime.Now}: {_actor.Identity} is trying to execute {command.Name} using data: {JsonConvert.SerializeObject(request)}");
+
+            if (!_actor.AllowedUseCases.Contains(command.Id))
+                throw new UnauthorizedUseCaseException(command, _actor);
 
             command.Execute(request);
         }
