3acdcb73debec26301c3e839eadd19f894ec0972
Aleksandar <aca801@gmail.com>
2020-09-25 19:19:15 +0200

    Add validator for group creation using Fluent Validation package

---
+++ DDDT.Implementation/Validators/CreateGroupValidator.cs
@@ -0,0 +1,21 @@
+﻿using DDDT.Application.DataTransfer;
+using DDDT.EfDataAccess;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DDDT.Implementation.Validators
+{
+    public class CreateGroupValidator : AbstractValidator<GroupDto>
+    {
+        public CreateGroupValidator(DDDTContext context)
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .Must(name => !context.Groups.Any(g => g.Name == name))
+                .WithMessage("Group name must be unique");
+        }
+    }
+}
