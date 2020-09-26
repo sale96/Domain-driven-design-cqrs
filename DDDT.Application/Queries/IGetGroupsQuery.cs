77353680965b876433b26c1e2a5e062bd6aed241
Aleksandar <aca801@gmail.com>
2020-09-26 21:34:27 +0200

    Add group query interface

---
+++ DDDT.Application/Queries/IGetGroupsQuery.cs
@@ -0,0 +1,12 @@
+﻿using DDDT.Application.DataTransfer;
+using DDDT.Application.Searches;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDT.Application.Queries
+{
+    public interface IGetGroupsQuery : IQuery<GroupSearch, IEnumerable<GroupDto>>
+    {
+    }
+}
