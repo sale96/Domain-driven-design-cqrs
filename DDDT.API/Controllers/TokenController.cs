85de98105870226e6c923d2da1550443e6125e22
Aleksandar <aca801@gmail.com>
2020-12-28 12:18:36 +0100

    Add Json web token controller

---
+++ DDDT.API/Controllers/TokenController.cs
@@ -0,0 +1,33 @@
+﻿using DDDT.API.Core;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DDDT.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly JwtManager _manager;
+
+        public TokenController(JwtManager manager)
+        {
+            _manager = manager;
+        }
+
+        // POST api/<TokenController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok(new
+            {
+                token = _manager.MakeToken("", "")
+            });
+        }
+    }
+}
