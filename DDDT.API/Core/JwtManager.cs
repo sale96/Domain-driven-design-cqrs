370b76a2a0df870309d13155cb130f511f19fe6f
Aleksandar <aca801@gmail.com>
2020-12-28 12:07:07 +0100

    Create Json web token manager class and token creator

---
+++ DDDT.API/Core/JwtManager.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DDDT.API.Core
+{
+    public class JwtManager
+    {
+        public string MakeToken(string username, string password)
+        {
+            var actor = new FakeApiActor();
+
+            var issuer = "asp_api";
+            var secretKey = "ThisIsAVerySecretKey";
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString(), ClaimValueTypes.String, issuer),
+                new Claim(JwtRegisteredClaimNames.Iss, "asp_api", ClaimValueTypes.String, issuer),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer, issuer),
+                new Claim("UserId", actor.Id.ToString(), ClaimValueTypes.String, issuer),
+                new Claim("ActorData", JsonConvert.SerializeObject(actor), ClaimValueTypes.String, issuer)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var now = DateTime.UtcNow;
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: "Any",
+                claims: claims,
+                notBefore: now,
+                expires: now.AddSeconds(30),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
